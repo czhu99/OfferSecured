f32f9b567addcacee6c51318acb70321471aae51
Chris Zhu <cthez72@gmail.com>
2021-12-01 19:21:00 -0600

    Adding user profile page

--- frontend/client/src/pages/UserPage.js
+++ frontend/client/src/pages/UserPage.js
@@ -1,9 +1,29 @@
 import React from "react";
 import "../css/UserPage.css"
+import defaultPhoto from '../assets/default_image.png'
+import {Card} from 'antd'
+import 'antd/dist/antd.css';
 
 function UserPage() {
+    let userInfo = {name: "User Name", photo: defaultPhoto}
     return (
-        <div> User </div>
+        <div>
+            <div className="profile-wrapper">
+                <div className="profile-header">
+                    <img className="profile-photo" src={defaultPhoto}></img>
+                    <div className=""> {userInfo.name} </div>
+                </div>
+
+                
+            </div>
+
+            <Card title="User Info">
+                <p>Industry:</p>
+                <p>Years of Experience:</p>
+                <p>Job Role:</p>
+                <p>Resume Link:</p>
+            </Card>
+        </div>
     )
 }
 
